ec6be26e5c733b0bf33cb4783c9ab8a6830ea696
mathe <mathe@LAPTOP-QU7ODQL0>
2024-11-04 21:16:41 +0100

    Première Version

---
+++ src/main/java/com/mycompany/calculatrice/Calculatrice.java
@@ -0,0 +1,9 @@
+package com.mycompany.calculatrice;
+
+import view.CalculatorGUI;
+
+public class Calculatrice {
+    public static void main(String[] args) {
+        CalculatorGUI.launch(CalculatorGUI.class, args);
+    }
+}
